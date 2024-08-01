d07e7b27e9bfdb918cf42b3cd6288a4696793b2b
Nino van der Mark <ninovdmark@gmail.com>
2024-08-01 11:49:22 +0200

    Added unit tests for Clear and Fill operations

--- ConsoleRenderer.Tests/Fill.cs
+++ ConsoleRenderer.Tests/Fill.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ConsoleRenderer.Tests
 {
-    internal class Fill
+    public class Fill
     {
+        [Theory]
+        [InlineData('x', ConsoleColor.Red, ConsoleColor.Blue)]
+        [InlineData('a', ConsoleColor.White, ConsoleColor.Gray)]
+        public void X_Y(char character, ConsoleColor foreground, ConsoleColor background)
+        {
+            // Assemble
+            const int width = 10;
+            const int height = 10;
+            var canvas = new ConsoleCanvas(width, height);
+
+            // Act
+            canvas.Fill(character, foreground, background);
+
+            // Assert
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var pixel = canvas.Get(x, y, false);
+                    Assert.Equal(foreground, pixel.Foreground);
+                    Assert.Equal(background, pixel.Background);
+                    Assert.Equal(character, pixel.Character);
+                }
+            }
+        }
     }
 }
