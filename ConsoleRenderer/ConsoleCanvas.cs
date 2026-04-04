bcd9a7247b0d59bd1345dab617ab44737e0ed7aa
Nino van der Mark <ninovdmark@gmail.com>
2026-04-04 18:40:09 +0200

    Added all current methods of ConsoleCanvas to ICanvas Added publish target for OSX Changed .NET version to .NET 8 Changed return type for all methods that returned ConsoleCanvas to ICanvas Moved method doc comments to interface

--- ConsoleRenderer/ConsoleCanvas.cs
+++ ConsoleRenderer/ConsoleCanvas.cs
@@ -65,23 +65,12 @@ namespace ConsoleRenderer
         {
         }
 
-        /// <summary>
-        /// Clears the canvas of all characters, using the default fore- and background colors
-        /// </summary>
-        /// <returns></returns>
-        public ConsoleCanvas Clear()
+        public ICanvas Clear()
         {
             return Fill(_emptyCharacter, DefaultForegroundColor, DefaultBackgroundColor);
         }
 
-        /// <summary>
-        /// Fills the canvas with the specified character in the given colors
-        /// </summary>
-        /// <param name="character">Character to fill the canvas with</param>
-        /// <param name="foreground">Foreground color</param>
-        /// <param name="background">Background color</param>
-        /// <returns></returns>
-        public ConsoleCanvas Fill(char character, ConsoleColor foreground, ConsoleColor background)
+        public ICanvas Fill(char character, ConsoleColor foreground, ConsoleColor background)
         {
             for (int y = 0; y < Height; y++)
                 for (int x = 0; x < Width; x++)
@@ -90,51 +79,22 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Creates a border on the edges of the canvas with the default fore- and background colors
-        /// </summary>
-        /// <param name="character">Character to draw the border with</param>
-        public ConsoleCanvas CreateBorder(char? character = null)
+        public ICanvas CreateBorder(char? character = null)
         {
             return CreateBorder(character, DefaultForegroundColor, DefaultBackgroundColor);
         }
 
-        /// <summary>
-        /// Creates a border on the edges of the canvas with the specified character and colors
-        /// </summary>
-        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
-        /// <param name="foreground">Color to draw the border with</param>
-        /// <param name="background">Color to draw the border with</param>
-        public ConsoleCanvas CreateBorder(char? character, ConsoleColor foreground, ConsoleColor background)
+        public ICanvas CreateBorder(char? character, ConsoleColor foreground, ConsoleColor background)
         {
             return CreateBorder(0, 0, Width, Height, character, foreground, background);
         }
 
-        /// <summary>
-        /// Creates a border on the edges of the canvas with the specified character and the default fore- and background colors
-        /// </summary>
-        /// <param name="startX">Left edge of the rectangle</param>
-        /// <param name="startY">Top edge of the rectangle</param>
-        /// <param name="width">Width of the rectangle</param>
-        /// <param name="height">Height of the rectangle</param>
-        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
-        public ConsoleCanvas CreateBorder(int startX, int startY, int width, int height, char? character = null)
+        public ICanvas CreateBorder(int startX, int startY, int width, int height, char? character = null)
         {
             return CreateBorder(startX, startY, width, height, character, DefaultForegroundColor, DefaultBackgroundColor);
         }
 
-        /// <summary>
-        /// Creates a border on the edges of a rectangle with the specified character and colors
-        /// </summary>
-        /// <param name="startX">Left edge of the rectangle</param>
-        /// <param name="startY">Top edge of the rectangle</param>
-        /// <param name="width">Width of the rectangle</param>
-        /// <param name="height">Height of the rectangle</param>
-        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
-        /// <param name="foreground">Color to draw the border with</param>
-        /// <param name="background">Color to draw the border with</param>
-        /// <returns></returns>
-        public ConsoleCanvas CreateBorder(int startX, int startY, int width, int height, char? character, ConsoleColor foreground, ConsoleColor background)
+        public ICanvas CreateBorder(int startX, int startY, int width, int height, char? character, ConsoleColor foreground, ConsoleColor background)
         {
             for (int y = startY; y < startY + height; y++)
             {
@@ -189,30 +149,12 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Creates a rectangle on the canvas 
-        /// </summary>
-        /// <param name="startX">Left edge of the rectangle</param>
-        /// <param name="startY">Top edge of the rectangle</param>
-        /// <param name="width">Width of the rectangle</param>
-        /// <param name="height">Height of the rectangle</param>
-        /// <param name="character">Character to fill the rectangle with</param>
-        public ConsoleCanvas CreateRectangle(int startX, int startY, int width, int height, char character = _defaultCharacter)
+        public ICanvas CreateRectangle(int startX, int startY, int width, int height, char character = _defaultCharacter)
         {
             return CreateRectangle(startX, startY, width, height, character, DefaultForegroundColor, DefaultBackgroundColor);
         }
 
-        /// <summary>
-        /// Creates a rectangle on the canvas 
-        /// </summary>
-        /// <param name="startX">Left edge of the rectangle</param>
-        /// <param name="startY">Top edge of the rectangle</param>
-        /// <param name="width">Width of the rectangle</param>
-        /// <param name="height">Height of the rectangle</param>
-        /// <param name="character">Character to fill the rectangle with</param>
-        /// <param name="foreground">Color to draw the character with</param>
-        /// <param name="background">Color to draw the background with</param>
-        public ConsoleCanvas CreateRectangle(int startX, int startY, int width, int height, char character, ConsoleColor foreground, ConsoleColor background)
+        public ICanvas CreateRectangle(int startX, int startY, int width, int height, char character, ConsoleColor foreground, ConsoleColor background)
         {
             for (int y = startY; y < Height && y-startY < height; y++)
             {
@@ -223,10 +165,7 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Renders all the pixels on the canvas
-        /// </summary>
-        public ConsoleCanvas Render()
+        public ICanvas Render()
         {
             Console.CursorTop = 0;
             Console.CursorLeft = 0;
@@ -336,13 +275,7 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Resizes the canvas to match the new dimensions
-        /// </summary>
-        /// <param name="width">The new <see cref="Width"/> of the <see cref="ConsoleCanvas"/></param>
-        /// <param name="height">The new <see cref="Height"/> of the <see cref="ConsoleCanvas"/></param>
-        /// <returns></returns>
-        public ConsoleCanvas Resize(int width, int height)
+        public ICanvas Resize(int width, int height)
         {
             Width = width;
             Height = height;
@@ -373,50 +306,22 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Set a particular pixel on the canvas to the specified character with the default fore- and background colors
-        /// </summary>
-        /// <param name="x">X Coordinate of the pixel</param>
-        /// <param name="y">Y Coordinate of the pixel</param>
-        /// <param name="character">Character to set the pixel to</param>
-        public ConsoleCanvas Set(int x, int y, char character = _defaultCharacter)
+        public ICanvas Set(int x, int y, char character = _defaultCharacter)
         {
             return Set(x, y, character, DefaultForegroundColor);
         }
 
-        /// <summary>
-        /// Set a particular pixel on the canvas to the specified foreground color, with the default background color
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="color"></param>
-        /// <returns></returns>
-        public ConsoleCanvas Set(int x, int y, ConsoleColor color)
+        public ICanvas Set(int x, int y, ConsoleColor color)
         {
             return Set(x, y, _defaultCharacter, color);
         }
 
-        /// <summary>
-        /// Set a particular pixel on the canvas to the specified character with a given color and the default background color
-        /// </summary>
-        /// <param name="x">X Coordinate of the pixel</param>
-        /// <param name="y">Y Coordinate of the pixel</param>
-        /// <param name="character">Character to set the pixel to</param>
-        /// <param name="color">Color to draw the character with</param>
-        public ConsoleCanvas Set(int x, int y, char character, ConsoleColor color)
+        public ICanvas Set(int x, int y, char character, ConsoleColor color)
         {
             return Set(x, y, character, color, DefaultBackgroundColor);
         }
 
-        /// <summary>
-        /// Set a particular pixel on the canvas to the specified character with a given background and foreground color
-        /// </summary>
-        /// <param name="x">X Coordinate of the pixel</param>
-        /// <param name="y">Y Coordinate of the pixel</param>
-        /// <param name="character">Character to set the pixel to</param>
-        /// <param name="foreground">Foreground color to draw the character with</param>
-        /// <param name="background">Background color to draw the character with</param>
-        public ConsoleCanvas Set(int x, int y, char character, ConsoleColor foreground, ConsoleColor background)
+        public ICanvas Set(int x, int y, char character, ConsoleColor foreground, ConsoleColor background)
         {
             return Set(x, y, new Pixel
             {
@@ -426,13 +331,7 @@ namespace ConsoleRenderer
             });
         }
 
-        /// <summary>
-        /// Set a particular pixel on the canvas to the specified <see cref="Pixel"/>
-        /// </summary>
-        /// <param name="x">X Coordinate of the pixel</param>
-        /// <param name="y">Y Coordinate of the pixel</param>
-        /// <param name="pixel">Pixel to set at the specified coordinates</param>
-        public ConsoleCanvas Set(int x, int y, Pixel pixel)
+        public ICanvas Set(int x, int y, Pixel pixel)
         {
             if (x >= 0 && x < Width && y >= 0 && y < Height)
                 _pixels[y][x] = pixel;
@@ -440,14 +339,7 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Draws a series of <see cref="Pixel"/>s to the screen, starting at the specified <paramref name="x"/> and <paramref name="y"/>
-        /// coordinates
-        /// </summary>
-        /// <param name="x">Starting x coordinate of the pixels, each consecutive one will be drawn to the right of the last</param>
-        /// <param name="y">Y Coordinate of the pixels</param>
-        /// <param name="pixels">Pixels to set at the specified coordinates</param>
-        public ConsoleCanvas Set(int x, int y, Pixel[] pixels)
+        public ICanvas Set(int x, int y, Pixel[] pixels)
         {
             for (int t = 0; t < pixels.Length; t++)
             {
@@ -457,14 +349,7 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Draws a series of <see cref="Pixel"/>s to the screen, starting at the specified <paramref name="x"/> and <paramref name="y"/>
-        /// coordinates
-        /// </summary>
-        /// <param name="x">Starting x coordinate of the pixels, each consecutive one will be drawn to the right of the last</param>
-        /// <param name="y">Y Coordinate of the pixels</param>
-        /// <param name="pixels">Pixels to set at the specified coordinates</param>
-        public ConsoleCanvas Set(int x, int y, List<Pixel> pixels)
+        public ICanvas Set(int x, int y, List<Pixel> pixels)
         {
             for (int t = 0; t < pixels.Count; t++)
             {
@@ -474,16 +359,7 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Draws the given <paramref name="text"/> to the canvas, starting at the <paramref name="x"/> and <paramref name="y"/> coordinates
-        /// </summary>
-        /// <param name="x">X Coordinate of the first character of the string</param>
-        /// <param name="y">Y Coordinate of the string</param>
-        /// <param name="text">The text to draw</param>
-        /// <param name="centered">Whether the text should be centered around the <paramref name="x"/> coordinate</param>
-        /// <param name="foreground">Foreground color to draw the string with, or <see cref="DefaultForegroundColor"/> if <see cref="null"/></param>
-        /// <param name="background">Background color to draw the string with, or <see cref="DefaultBackgroundColor"/> if <see cref="null"/></param>
-        public ConsoleCanvas Text(int x, int y, string text, bool centered = false, ConsoleColor? foreground = null, ConsoleColor? background = null)
+        public ICanvas Text(int x, int y, string text, bool centered = false, ConsoleColor? foreground = null, ConsoleColor? background = null)
         {
             // If the text should be centered, deduct half the text length from the x coordinate
             int startX = centered ? x - (int) Math.Floor(text.Length/2d) : x;
@@ -501,12 +377,6 @@ namespace ConsoleRenderer
             return this;
         }
 
-        /// <summary>
-        /// Returns the <see cref="Pixel"/> at the given <paramref name="x"/>,<paramref name="y"/> coordinates
-        /// </summary>
-        /// <param name="backBuffer">Whether to return the pixel as it was last drawn (<see cref="true"/>), or the
-        /// one that will be drawn at the next call to <see cref="Render"/></param>
-        /// <exception cref="IndexOutOfRangeException"></exception>
         public Pixel Get(int x, int y, bool backBuffer = true)
         {
             if ( x < 0 || y < 0 || x >= Width || y >= Height)
--- ConsoleRenderer/ICanvas.cs
+++ ConsoleRenderer/ICanvas.cs
@@ -39,6 +39,88 @@ namespace ConsoleRenderer
         bool Interlaced { get; set; }
 
         /// <summary>
+        /// Clears the canvas of all characters, using the default fore- and background colors
+        /// </summary>
+        /// <returns>A reference to this instance of <see cref="ICanvas"/></returns>
+        ICanvas Clear();
+
+        /// <summary>
+        /// Creates a border on the edges of the canvas with the default fore- and background colors
+        /// </summary>
+        /// <param name="character">Character to draw the border with</param>
+        ICanvas CreateBorder(char? character = null);
+
+        /// <summary>
+        /// Creates a border on the edges of the canvas with the specified character and colors
+        /// </summary>
+        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
+        /// <param name="foreground">Color to draw the border with</param>
+        /// <param name="background">Color to draw the border with</param>
+        ICanvas CreateBorder(char? character, ConsoleColor foreground, ConsoleColor background);
+
+        /// <summary>
+        /// Creates a border on the edges of the canvas with the specified character and the default fore- and background colors
+        /// </summary>
+        /// <param name="startX">Left edge of the rectangle</param>
+        /// <param name="startY">Top edge of the rectangle</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
+        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
+        ICanvas CreateBorder(int startX, int startY, int width, int height, char? character = null);
+
+        /// <summary>
+        /// Creates a border on the edges of a rectangle with the specified character and colors
+        /// </summary>
+        /// <param name="startX">Left edge of the rectangle</param>
+        /// <param name="startY">Top edge of the rectangle</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
+        /// <param name="character">Character to draw the border with, or <see cref="null"/> to use default pretty borders</param>
+        /// <param name="foreground">Color to draw the border with</param>
+        /// <param name="background">Color to draw the border with</param>
+        /// <returns></returns>
+        ICanvas CreateBorder(int startX, int startY, int width, int height, char? character, ConsoleColor foreground, ConsoleColor background);
+
+        /// <summary>
+        /// Creates a rectangle on the canvas 
+        /// </summary>
+        /// <param name="startX">Left edge of the rectangle</param>
+        /// <param name="startY">Top edge of the rectangle</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
+        /// <param name="character">Character to fill the rectangle with</param>
+        ICanvas CreateRectangle(int startX, int startY, int width, int height, char character = '*');
+
+        /// <summary>
+        /// Creates a rectangle on the canvas 
+        /// </summary>
+        /// <param name="startX">Left edge of the rectangle</param>
+        /// <param name="startY">Top edge of the rectangle</param>
+        /// <param name="width">Width of the rectangle</param>
+        /// <param name="height">Height of the rectangle</param>
+        /// <param name="character">Character to fill the rectangle with</param>
+        /// <param name="foreground">Color to draw the character with</param>
+        /// <param name="background">Color to draw the background with</param>
+        ICanvas CreateRectangle(int startX, int startY, int width, int height, char character, ConsoleColor foreground, ConsoleColor background);
+
+        /// <summary>
+        /// Fills the canvas with the specified character in the given colors
+        /// </summary>
+        /// <param name="character">Character to fill the canvas with</param>
+        /// <param name="foreground">Foreground color</param>
+        /// <param name="background">Background color</param>
+        /// <returns></returns>
+        ICanvas Fill(char character, ConsoleColor foreground, ConsoleColor background);
+
+        /// <summary>
+        /// Returns the <see cref="Pixel"/> at the given <paramref name="x"/>,<paramref name="y"/> coordinates
+        /// </summary>
+        /// <param name="backBuffer">Whether to return the pixel as it was last drawn (<see cref="true"/>), or the
+        /// one that will be drawn at the next call to <see cref="Render"/></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        Pixel Get(int x, int y, bool backBuffer = true);
+
+        /// <summary>
         /// Resizes the canvas to match the new dimensions
         /// </summary>
         /// <param name="width">The new <see cref="Width"/> of the <see cref="ICanvas"/></param>
@@ -53,9 +135,76 @@ namespace ConsoleRenderer
         ICanvas Render();
 
         /// <summary>
-        /// Clears the canvas of all characters, using the default fore- and background colors
+        /// Set a particular pixel on the canvas to the specified character with the default fore- and background colors
         /// </summary>
-        /// <returns>A reference to this instance of <see cref="ICanvas"/></returns>
-        ICanvas Clear();
+        /// <param name="x">X Coordinate of the pixel</param>
+        /// <param name="y">Y Coordinate of the pixel</param>
+        /// <param name="character">Character to set the pixel to</param>
+        ICanvas Set(int x, int y, char character = '*');
+
+        /// <summary>
+        /// Set a particular pixel on the canvas to the specified foreground color, with the default background color
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        ICanvas Set(int x, int y, ConsoleColor color);
+
+        /// <summary>
+        /// Set a particular pixel on the canvas to the specified character with a given color and the default background color
+        /// </summary>
+        /// <param name="x">X Coordinate of the pixel</param>
+        /// <param name="y">Y Coordinate of the pixel</param>
+        /// <param name="character">Character to set the pixel to</param>
+        /// <param name="color">Color to draw the character with</param>
+        ICanvas Set(int x, int y, char character, ConsoleColor color);
+
+        /// <summary>
+        /// Set a particular pixel on the canvas to the specified character with a given background and foreground color
+        /// </summary>
+        /// <param name="x">X Coordinate of the pixel</param>
+        /// <param name="y">Y Coordinate of the pixel</param>
+        /// <param name="character">Character to set the pixel to</param>
+        /// <param name="foreground">Foreground color to draw the character with</param>
+        /// <param name="background">Background color to draw the character with</param>
+        ICanvas Set(int x, int y, char character, ConsoleColor foreground, ConsoleColor background);
+
+        /// <summary>
+        /// Set a particular pixel on the canvas to the specified <see cref="Pixel"/>
+        /// </summary>
+        /// <param name="x">X Coordinate of the pixel</param>
+        /// <param name="y">Y Coordinate of the pixel</param>
+        /// <param name="pixel">Pixel to set at the specified coordinates</param>
+        ICanvas Set(int x, int y, Pixel pixel);
+
+        /// <summary>
+        /// Draws a series of <see cref="Pixel"/>s to the screen, starting at the specified <paramref name="x"/> and <paramref name="y"/>
+        /// coordinates
+        /// </summary>
+        /// <param name="x">Starting x coordinate of the pixels, each consecutive one will be drawn to the right of the last</param>
+        /// <param name="y">Y Coordinate of the pixels</param>
+        /// <param name="pixels">Pixels to set at the specified coordinates</param>
+        ICanvas Set(int x, int y, Pixel[] pixels);
+
+        /// <summary>
+        /// Draws a series of <see cref="Pixel"/>s to the screen, starting at the specified <paramref name="x"/> and <paramref name="y"/>
+        /// coordinates
+        /// </summary>
+        /// <param name="x">Starting x coordinate of the pixels, each consecutive one will be drawn to the right of the last</param>
+        /// <param name="y">Y Coordinate of the pixels</param>
+        /// <param name="pixels">Pixels to set at the specified coordinates</param>
+        ICanvas Set(int x, int y, List<Pixel> pixels);
+
+        /// <summary>
+        /// Draws the given <paramref name="text"/> to the canvas, starting at the <paramref name="x"/> and <paramref name="y"/> coordinates
+        /// </summary>
+        /// <param name="x">X Coordinate of the first character of the string</param>
+        /// <param name="y">Y Coordinate of the string</param>
+        /// <param name="text">The text to draw</param>
+        /// <param name="centered">Whether the text should be centered around the <paramref name="x"/> coordinate</param>
+        /// <param name="foreground">Foreground color to draw the string with, or <see cref="DefaultForegroundColor"/> if <see cref="null"/></param>
+        /// <param name="background">Background color to draw the string with, or <see cref="DefaultBackgroundColor"/> if <see cref="null"/></param>
+        ICanvas Text(int x, int y, string text, bool centered = false, ConsoleColor? foreground = null, ConsoleColor? background = null);
     }
 }
